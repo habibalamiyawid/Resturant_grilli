25ddb905af26b4469895b84ca143f141fc6a739b
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-09-05 16:25:39 +0600

    navbar header added

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+/** 
+ * preload loading will be end after document is loaded
+ * 
+ * **/
+
+const preloader = document.querySelector("[data-preaload]");
+
+window.addEventListener("load", function()
+{
+    preloader.classList.add("loaded");
+    document.body.classList.add("loaded");
+
+});
