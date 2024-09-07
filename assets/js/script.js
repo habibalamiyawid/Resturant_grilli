ef3ef0c5d19df60028bf612e1a022a60c9ea8c6f
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-09-07 09:27:06 +0600

    navmenu update

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,15 @@ window.addEventListener("load", function()
     document.body.classList.add("loaded");
 
 });
+
+/**
+ * add event listener on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) 
+{
+    for (let i = 0, len= elements.length; i < len; i++)
+{
+ elements[i].addEventListener(eventType, callback);
+}
+}
