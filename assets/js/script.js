07fae55e83f61ecea371e6edae0212b87d56b142
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-09-14 20:15:13 +0600

    working

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,4 +71,61 @@ window.addEventListener("scroll", function(){
         header.classList.remove("active");
     }
 
-})
+});
+
+// hero slider
+const heroSlider = document.querySelector("[data-hero-slider]");
+const heroSliderItems = document.querySelectorAll("[data-hero-slider-item]");
+const heroSliderPrevBtn = document.querySelector("[data-prev-btn]");
+
+let currentSlidePos = 0;
+let lastActiveSliderItem = heroSliderItems[0];
+
+const updateSliderPos = function (){
+    lastActiveSliderItem.classList.remove("active");
+    heroSliderItems[currentSlidePos].classList.add("active");
+    lastActiveSliderItem=heroSliderItems[currentSlidePos];                                    
+}
+
+const slideNext = function (){
+    if(currentSlidePos >= heroSliderItems.length - 1)
+    {
+        currentSlidePos = 0;
+    }
+    else
+    {
+        currentSlidePos++;
+    }
+    updateSliderPos();
+}
+
+const slidePrev = function (){
+    if(currentSlidePos <= 0)
+    {
+        currentSlidePos = heroSliderItems.length - 1;
+    }
+    else
+    {
+        currentSlidePos--;
+    }
+    updateSliderPos();
+}
+
+heroSliderPrevBtn.addEventListener("click", slideNext);
+// autoslider section
+let autoSlideInterval;
+
+const autoSlide = function (){
+    autoSlideInterval = setInterval(function(){
+        slideNext();
+    }, 7000);
+}
+
+addEventOnElements([heroSliderNextBtn , heroSliderPrevBtn],"mouseover",function(){
+    clearInterval(autoSlideInterval);
+});
+
+addEventOnElements([heroSliderNextBtn,heroSliderPrevBtn],"mouseout",autoSlide);
+
+window.addEventListener("load",autoSlide);
+
