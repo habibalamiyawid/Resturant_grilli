6391ade5f5d65d02427dcbb48e0898f5a74be428
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-10-02 00:43:37 +0600

    back to top button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,10 +42,13 @@ const toggleNavbar = function ()
 addEventOnElements(navTogglers, "click", toggleNavbar);
 
 /**
- * Header
+ * Header & Back top button
  */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn= document.querySelector(["data-back-top-btn"]);
+
+
 let lastScrollPos = 0;
 
 const hideHeader = function () {
@@ -64,11 +67,13 @@ window.addEventListener("scroll", function(){
     if (window.scrollY >= 50)
     {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
         hideHeader();
     }
     else
     {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 
 });
